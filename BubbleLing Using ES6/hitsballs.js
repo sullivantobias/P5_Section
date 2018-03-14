e9ec6d0a09fbff935da44f6cddbdd94dec0160c2
Tobias <githubst@gmail.com>
2018-03-14 23:44:48 +0100

    Addings

--- BubbleLing Using ES6/hitsballs.js
+++ BubbleLing Using ES6/hitsballs.js
@@ -9,14 +9,30 @@ function setup() {
     let b = new Bubble(random(width), random(height), r);
     ar.push(b);
 
-}**/
+} ** /
+/** Test Bubble on react to the click and hover state **/
+  for (var i = 0; i < 30; i++) {
+    let r = random(10, 40);
+    let b = new Bubble(random(width), random(height), r);
+    ar.push(b);
+  }
 }
-
+/*
 function mouseDragged() {
-  /** If we want to made bubbles each time we drag**/
+  /* If we want to made bubbles each time we drag */
+/*
   let r = random(10, 40);
   let b = new Bubble(mouseX, mouseY, r);
   ar.push(b);
+
+}
+*/
+function mousePressed() {
+  for (let i = 0; i < ar.length; i++) {
+    if (ar[i].isClicked(mouseX, mouseY)) {
+      ar.splice(i, 1);
+    }
+  }
 }
 
 function draw() {
@@ -24,7 +40,16 @@ function draw() {
   for (let i = 0; i < ar.length; i++) {
     ar[i].displayed();
     ar[i].moved();
-    ar[i].blow();
+
+    if (ar[i].blow()) {
+      ar.splice(i, 1);
+    }
+    if (ar[i].isHovered(mouseX, mouseY)) {
+      ar[i].strokeChange("red");
+    } else {
+      ar[i].strokeChange(255);
+    }
+
   }
 }
 
@@ -33,6 +58,28 @@ class Bubble {
     this.x = x;
     this.y = y;
     this.r = radius;
+    this.strokeColor = 255;
+  }
+  strokeChange(color) {
+    this.strokeColor = color;
+  }
+
+  isHovered(x, y) {
+    var d = dist(x, y, this.x, this.y)
+    if (d < this.r) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isClicked(x, y) {
+    var d = dist(x, y, this.x, this.y)
+    if (d < this.r) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   displayed() {
@@ -44,7 +91,8 @@ class Bubble {
       let b = random(0, 255);
       stroke(r, g, b);
       strokeWeight(3); */
-    noStroke();
+
+    stroke(this.strokeColor);
     fill(255, 50);
     ellipse(this.x, this.y, this.r);
   }
@@ -56,7 +104,9 @@ class Bubble {
   blow() {
     if (this.x < this.r / 2 || this.x > width - (this.r / 2) ||
       this.y < this.r / 2 || this.y > height - (this.r / 2)) {
-      this.r = 0;
+      return true;
+    } else {
+      return false;
     }
   }
 }
